50d25a76a5f7ec9c38fb308cfa39a85794399be3
forest-shadow <mamasay@yandex.ru>
2017-07-23 12:15:12 +0300

    Factory Pattern: little structure refactoring; adding comments for better understanding whats going on

--- js/factory/init.js
+++ js/factory/init.js
@@ -4,21 +4,21 @@ define(function( require ) {
     return {
         init: function() {
             var myVideo, myImage,
-                mediaFactory = require('factory/mediaFactory');
+                mediaFactory = require( 'factory/mediaFactory' );
 
-            myVideo = mediaFactory.createMedia('Video', {
+            myVideo = mediaFactory.createMedia( 'Video', {
                 length: 3.5,
                 name: 'My video'
             });
 
-            myImage = mediaFactory.createMedia('Image', {
+            myImage = mediaFactory.createMedia( 'Image', {
                 width: 100,
                 height: 100,
                 name: 'My Image'
             });
 
-            console.log(myVideo);
-            console.log(myImage);
+            console.log( myVideo );
+            console.log( myImage );
         }
     };
 });
--- js/factory/mediaFactory.js
+++ js/factory/mediaFactory.js
@@ -1,14 +1,17 @@
 define( function( require ) {
     'use strict';
 
+    // This factory will create objects of different objects types
+
+    // object with media types Constructors
     var media = {
-      Video: require('factory/video'),
-      Image: require('factory/image')
+      Video: require( 'factory/mediaConstructors/video' ),
+      Image: require( 'factory/mediaConstructors/image' )
     };
 
     return {
         createMedia: function( type, attributes ) {
-            var MediaType = media[type];
+            var MediaType = media[ type ];
 
             return new MediaType( attributes );
         }
